326a5bfb9346a00ea2073f4fa5613e122b0def46
Unknown <jonathanbae7@gmail.com>
2019-07-27 16:08:25 -0600

    basic timer

--- stretcher/lib/src/components/workout/workout_main.dart
+++ stretcher/lib/src/components/workout/workout_main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:stretcher/src/models/workout_stretch_object.dart';
 import 'package:stretcher/src/static_files/static_styles.dart';
@@ -13,22 +16,81 @@ class WorkoutView extends StatefulWidget {
   WorkoutViewState createState() => WorkoutViewState();
 }
 
-class WorkoutViewState extends State<WorkoutView> {
+class WorkoutViewState extends State<WorkoutView> with TickerProviderStateMixin {
+  AnimationController controller;
+
+  String get timerString {
+    Duration duration = controller.duration * controller.value;
+    return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 10),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.selectorStore.currentWeek + ' ' + widget.selectorStore.currentDay),
       ),
-      body: _buildDayList(),
+      body: Column(
+        children: <Widget>[
+          new Expanded(child: _buildTimer()),
+          new Expanded(child: _buildTimerStartStop()),
+          new Expanded(child: _buildDayList()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTimer() {
+    return AnimatedBuilder(
+        animation: controller,
+        builder: (BuildContext context, Widget child) {
+          return Text(
+            timerString,
+            style: StretcherStyles().biggerFont,
+          );
+        });
+  }
+
+  Widget _buildTimerStartStop() {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          FloatingActionButton(
+            child: AnimatedBuilder(
+              animation: controller,
+              builder: (BuildContext context, Widget child) {
+                return Icon(controller.isAnimating ? Icons.pause : Icons.play_arrow);
+              },
+            ),
+            onPressed: () {
+              if (controller.isAnimating)
+                controller.stop();
+              else {
+                controller.reverse(from: controller.value == 0.0 ? 1.0 : controller.value);
+              }
+            },
+          )
+        ],
+      ),
     );
   }
 
   Widget _buildDayList() {
     return ListView.separated(
         separatorBuilder: (context, index) => Divider(
-          color: Colors.black,
-        ),
+              color: Colors.black,
+            ),
         padding: const EdgeInsets.all(16.0),
         itemCount: widget.workoutOfDay.length,
         itemBuilder: _buildDateRow);
@@ -44,8 +106,7 @@ class WorkoutViewState extends State<WorkoutView> {
       ),
       subtitle: Text(currentWorkout.leftRight + ' ' + currentWorkout.superSetNumber),
       trailing: Text(currentWorkout.time),
-      onTap: () {
-      },
+      onTap: () {},
     );
   }
 }
